8456a04049a00e9a0ce4b51a95fd6291b5c6cdab
Jed Bartolome <jed.bartolome17@gmail.com>
2024-07-01 01:19:18 +0800

    Add initial file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Portfolio';
+  constructor() { }
+
+  imports!: [
+    NgbModule
+  ];
 }
